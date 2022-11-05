56002685f10962409c8120923efc35c4bc8970cc
Sharp <37125840+ItsSharples@users.noreply.github.com>
2022-11-05 11:14:43 +0000

    Changed to Pattern Matching. Doesn't reach memory limits as quickly. Also changed to give actual answers instead of string of equation

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,27 @@
-def generate(size):
-    output = "def calculate(lh,rh,op):";
-    len_ot = len(output) + 2
+def generate(size, justAnswer = True):
+    if justAnswer: return generate_patterns_just_answer(size);
+    else: return generate_patterns(size);
+    
+    
+def generate_patterns(size):
+    output = "def calculate(lh,rh,op):\n\tmatch [lh, op, rh]:";
+    len_ot = len(output)
     operators = ["+","-","*","/"];
     for op in operators:
         for lh in range(size):
             start = 1 if op == "/" else 0;
             for rh in range(start, size):
-                output += ("\n\telif ({lh} == lh and \"{op}\" == op and {rh} == rh):\n\t\treturn \"{lh}{op}{rh} = ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh)))+"\"")
-    return output[0: len_ot] + output[len_ot + 2:]
-    
+                output += ("\n\t\tcase [{lh},\"{op}\",{rh}]:\n\t\t\treturn \"{lh}{op}{rh} = ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh)))+"\"")
+    return output
+
+def generate_patterns_just_answer(size):
+    output = "def calculate(lh,rh,op):\n\tmatch [lh, op, rh]:";
+    len_ot = len(output)
+    operators = ["+","-","*","/"];
+    for op in operators:
+        for lh in range(size):
+            start = 1 if op == "/" else 0;
+            for rh in range(start, size):
+                output += ("\n\t\tcase [{lh},\"{op}\",{rh}]:\n\t\t\treturn ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh))))
+    return output
     
--- Example.py
+++ Example.py
@@ -1,10 +1,10 @@
 import Calculator
 
-output = Calculator.generate(27)
+output = Calculator.generate(100)
 
 with open("Output.py", mode = "w") as file:
   file.write(output)
   
 import Output
  
-assert Output.calculate(10,10,"+") == "10+10 = 20", "The Generator is Borked oops"
+assert Output.calculate(10,10,"+") == 20, "The Generator is Borked oops"
