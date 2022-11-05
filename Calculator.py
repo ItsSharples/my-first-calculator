0154c8961727be432539d8a2b6a8bb9e8614caa2
Sharp <37125840+ItsSharples@users.noreply.github.com>
2022-11-05 11:22:23 +0000

    Cleaned up Calculator Added Test for both versions of output.

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,13 @@
 def generate(size, justAnswer = True):
-    if justAnswer: return generate_patterns_just_answer(size);
-    else: return generate_patterns(size);
-    
-    
-def generate_patterns(size):
     output = "def calculate(lh,rh,op):\n\tmatch [lh, op, rh]:";
-    len_ot = len(output)
     operators = ["+","-","*","/"];
     for op in operators:
-        for lh in range(size):
+        for lh in range(size + 1):
             start = 1 if op == "/" else 0;
-            for rh in range(start, size):
-                output += ("\n\t\tcase [{lh},\"{op}\",{rh}]:\n\t\t\treturn \"{lh}{op}{rh} = ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh)))+"\"")
+            for rh in range(start, size + 1):
+                output += "\n\t\tcase [{lh},\"{op}\",{rh}]:\n\t\t\treturn ".format(lh = lh, op = op, rh = rh)
+                if justAnswer:
+                    output += str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh)))
+                else:
+                    output += "\"{lh}{op}{rh} = ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh)))+"\""
     return output
-
-def generate_patterns_just_answer(size):
-    output = "def calculate(lh,rh,op):\n\tmatch [lh, op, rh]:";
-    len_ot = len(output)
-    operators = ["+","-","*","/"];
-    for op in operators:
-        for lh in range(size):
-            start = 1 if op == "/" else 0;
-            for rh in range(start, size):
-                output += ("\n\t\tcase [{lh},\"{op}\",{rh}]:\n\t\t\treturn ".format(lh = lh, op = op, rh = rh)+str(eval("{lh}{op}{rh}".format(lh=lh, op=op, rh=rh))))
-    return output
-    
--- Example.py
+++ Example.py
@@ -1,10 +1,12 @@
 import Calculator
 
-output = Calculator.generate(100)
+output_true = Calculator.generate(100, True)
+output_false = Calculator.generate(100, False)
+with open("JustAnswers.py", mode = "w") as file:
+  file.write(output_true)
+with open("Equations.py", mode = "w") as file:
+  file.write(output_false)
 
-with open("Output.py", mode = "w") as file:
-  file.write(output)
-  
-import Output
- 
-assert Output.calculate(10,10,"+") == 20, "The Generator is Borked oops"
+import JustAnswers, Equations
+assert JustAnswers.calculate(10,10,"+") == 20, "The Generator is Borked oops"
+assert Equations.calculate(10,10,"+") == "10+10 = 20", "The Generator is Borked oops"
